b18bcf1042e030521cd60c191f914c49fc7fe4a4
Ha Dinh <dinhhn.ubc@gmail.com>
2018-03-01 15:47:18 -0800

    Fixed test function. Turn into class.

--- RegscorePy/test/test_aic.py
+++ RegscorePy/test/test_aic.py
@@ -1,7 +1,8 @@
 ## Add code to test AIC function
-def test_y_ypred_len():
-    """
-    test if y and y_pred have same length, if not yield error
-    """
-    with pytest.raises(Error):
-        RegscorePy.aic([1,2,3,4],[4,5,6,7,8], 3)
+class test_aic:
+    def y_ypred_len():
+        """
+        test if y and y_pred have same length, if not yield error
+        """
+        with pytest.raises(Error):
+            RegscorePy.aic([1,2,3,4],[4,5,6,7,8], 3)

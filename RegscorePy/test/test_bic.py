64606fe8c652049f0d7c45fdde3258ff764d3336
Simran Sethi <simrannsethi@gmail.com>
2018-03-01 15:46:47 -0800

    added test aic

--- RegscorePy/test/test_bic.py
+++ RegscorePy/test/test_bic.py
@@ -1,1 +1,15 @@
-## add BIC test cases
+import pytest
+from RegscorePy import RegscorePy
+
+
+class Test_bic:
+
+    def test_type_y_ypred(self):
+    ```
+    check if y and y_pred are vector
+    ```
+        with pytest.raises(TypeError):
+            RegscorePy.bic("a", [[1,2],[2,2]], 3.3)
+
+
+

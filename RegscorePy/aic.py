c2b029479e22928a08cf968bb5cc7cf145f785fe
Ha Dinh <dinhhn.ubc@gmail.com>
2018-03-02 12:24:45 -0800

    Set up function documentary

--- RegscorePy/aic.py
+++ RegscorePy/aic.py
@@ -1,4 +1,14 @@
 def aic(y, y_pred, p):
     """
-    
+    Return an AIC score for a model.
+    Input:
+    y: array-like of shape = (n_samples) including values of observed y
+    y_pred: vector including values of predicted y
+    p: int number of predictive variable(s) used in the model
+
+    Raise Error if y and y_pred are not in same length.
+    Raise Error if length(y) <= 1 or length(y_pred) <= 1.
+    Raise TypeError if y and y_pred are not vector.
+    Raise TypeError if p is not int.
+    Raise Error if p < 0.
     """
--- RegscorePy/test/test_aic.py
+++ RegscorePy/test/test_aic.py
@@ -9,3 +9,8 @@ class Test_aic:
         """
         with pytest.raises(Error):
             RegscorePy.aic([1,2,3,4],[4,5,6,7,8], 3)
+
+    def type_p():
+        """
+
+        """

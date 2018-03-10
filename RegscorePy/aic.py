cf2b97ebd9dfc17ae5fc49642bf9299b24ccf329
Ha Dinh <dinhhn.ubc@gmail.com>
2018-03-09 19:50:30 -0800

    Fixed aic() documentation for errors

--- RegscorePy/aic.py
+++ RegscorePy/aic.py
@@ -6,11 +6,12 @@ def aic(y, y_pred, p):
     y_pred: vector including values of predicted y
     p: int number of predictive variable(s) used in the model
 
-    Raise Error if y and y_pred are not in same length.
-    Raise Error if length(y) <= 1 or length(y_pred) <= 1.
-    Raise TypeError if y and y_pred are not vector.
+    Raise TypeError if y or y_pred are not list/tuple/dataframe column/array.
+    Raise TypeError if elements in y or y_pred are not integer or float.
     Raise TypeError if p is not int.
-    Raise Error if p < 0.
+    Raise InputError if y or y_pred are not in same length.
+    Raise InputError if length(y) <= 1 or length(y_pred) <= 1.
+    Raise InputError if p < 0.
     """
 
     # Package dependencies

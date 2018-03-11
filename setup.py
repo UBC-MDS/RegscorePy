13ffb8a01f1475e0d5c9d09e8b77a23f97ceb512
Stephanie <32108884+rq1995@users.noreply.github.com>
2018-03-11 09:13:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = 'Ha Dinh, Simran Sethi, Ruoqi Xu',
     author_email = 'dinhhn.ubc@gmail.com, simran.sethi@alumni.ubc.ca, rq658182@dal.ca',
     packages = ['RegscorePy'],
-    scripts = ['RegscorePy/aic.py', 'RegscorePy/bic.py', 'RegscorePy/mallows.py'],
+    scripts = ['RegscorePy/aic.py', 'RegscorePy/bic.py', 'RegscorePy/mallow.py'],
     url = 'https://github.com/UBC-MDS/regscore-py',
     license = 'LICENSE',
     description = 'Useful score functions to assist regression model comparison',

58b76c242f1d85de2a82469ba7ae0757be4c4619
RUOQI XU <7Ymr@dhcp-128-189-237-28.ubcsecure.wireless.ubc.ca>
2018-03-16 21:40:28 -0700

    Add inputerror in test mallow

--- tests/test_mallows.py
+++ tests/test_mallows.py
@@ -42,14 +42,14 @@ class Test_mallow:
         """
         check if p less than k
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1, 2, 3, 4], [3, 5, 5, 3], [5, 6, 7, 7], 2, 3)
 
     def test_positive_p(self):
         """
         check if p is positive
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1, 2, 3, 4], [5, 6, 7, 8], [5, 6, 7, 8], 3, -1)
 
     def test_integer_p(self):
@@ -63,7 +63,7 @@ class Test_mallow:
         """
         check if k is positive
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1, 2, 3, 4], [5, 6, 7, 8], [5, 6, 7, 8], -1, 3)
 
     def test_integer_k(self):
@@ -98,33 +98,33 @@ class Test_mallow:
         """
         check if y, and y_pred have same length
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1, 2], [1, 2, 3], [1, 2], 3, 2)
 
     def test_match_y_ysub(self):
         """
         check if y, and y_sub have same length
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1, 2], [1, 2], [1, 2, 3], 3, 2)
 
     def test_length_y_ypred(self):
         """
         check if the length of y, y_sub, and y_pred is 1
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1], [3], [2], 3, 2)
 
     def test_empty_y(self):
         """
         check if the y is empty
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([],[1,2,3,4],[5,6,7,8],3,2)
 
     def test_empty_y_pred(self):
         """
         check if the y_pred is empty
         """
-        with pytest.raises(Exception):
+        with pytest.raises(InputError):
             mallow([1,2,3,4],[],[5,6,7,8],3,2)

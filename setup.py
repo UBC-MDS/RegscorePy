7fe320972a92bc45bec26c73a3d2dd21f1ad0116
Simran Sethi <simrannsethi@gmail.com>
2018-03-17 22:12:01 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -10,11 +10,12 @@ setup(
     packages = ['RegscorePy'],
     scripts = ['RegscorePy/aic.py', 'RegscorePy/bic.py', 'RegscorePy/mallow.py'],
     url = 'https://github.com/UBC-MDS/regscore-py',
-    license = 'LICENSE',
+    license = 'MIT',
     description = 'Useful score functions to assist regression model comparison',
     long_description = open('README.md').read(),
     install_requires = ['numpy','pandas'],
     include_package_data=True,
     download_url = 'https://github.com/UBC-MDS/RegscorePy/archive/0.1.tar.gz',
-    keywords = ['aic','bic','mallows','regression','scores','machine-learning']
+    keywords = ['aic','bic','mallows','regression-score','machine-learning'],
+    classifiers = [],
 )
